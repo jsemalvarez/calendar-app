5c15589901996c5979ab43cad7706a1c87ba3513
Jose Alvarez <jalvarez@maker.local>
2020-09-21 14:34:40 -0300

    302.Presonalizar el cuaro y disparar eventos del calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Calendar, momentLocalizer} from 'react-big-calendar'
 import moment from 'moment'
 
 import { NavBar } from '../../ui/NavBar'
 import { messages } from '../helpers/calendar-messages-es'
+import { CalendarEvent } from './CalendarEvent'
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import 'moment/locale/es'
@@ -16,10 +17,31 @@ const myEventsList = [{
     title: 'Cumpleaños del jefe',
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#fafafa',
+    notes: ' Comer torta',
+    user: {
+        _id:'123',
+        name: 'Jose'
+    }
 }]
 
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month')
+
+    const onDoubleClick = (e) => {
+        console.log(e)
+    }
+
+    const onSelectEvent = (e) => {
+        console.log(e)
+    }
+
+    const onViewChange = (e) => {
+        console.log(e)
+        setLastView(e)
+        localStorage.setItem('lastView', e)
+    }
 
     const eventStyleGetter = ( event, start, end, isSelected ) => {
         console.log(event, start, end, isSelected)
@@ -47,6 +69,13 @@ export const CalendarScreen = () => {
                 endAccessor="end"
                 messages={ messages }
                 eventPropGetter={ eventStyleGetter }
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelectEvent }
+                onView={ onViewChange }
+                view={ lastView }
+                components={{
+                    event: CalendarEvent
+                }}
             />
         </div>
     )
